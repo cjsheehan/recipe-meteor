80a3082a744c3691d137d3933b39f49afccee65e
Chris Sheehan <chris.sheehan@ymail.com>
2016-07-15 20:45:00 +0100

    Control recipe subscriptions between book/all and single recipe

--- server/publish.js
+++ server/publish.js
@@ -2,3 +2,9 @@ Meteor.publish('recipes', function(){
   // only current users recipes will be available to them
   return Recipes.find({author: this.userId})
 });
+
+Meteor.publish('singleRecipe', function(id){
+  // only current users recipes will be available to them
+  check(id, String);
+  return Recipes.find({_id: id})
+});
