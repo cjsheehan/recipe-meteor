9d4079bd1b7d0e54c160010464d31e567f8df3dd
Chris Sheehan <chris.sheehan@ymail.com>
2016-07-18 16:46:33 +0100

    added delete recipe, updated styles

--- client/recipes/Recipe.js
+++ client/recipes/Recipe.js
@@ -1,5 +1,15 @@
+Template.Recipe.helpers({
+  updateRecipeId: function() {
+    return this._id;
+  }
+});
+
+
 Template.Recipe.events({
   'click .toggle-menu': function() {
     Meteor.call('toggleMenuItem', this._id, this.inMenu);
+  },
+  'click .fa-trash' : function() {
+    Meteor.call('deleteRecipe', this._id);
   }
 });
